061041864018cb2edf3a0b33571ef3472df07825
TanyaAgarwal92 <55943446+TanyaAgarwal92@users.noreply.github.com>
2019-11-16 13:04:21 +0530

    Update TwoSum.java

--- TwoSum.java
+++ TwoSum.java
@@ -1,6 +1,6 @@
-Problem : Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+/*Problem : Given an array of integers, return indices of the two numbers such that they add up to a specific target.
 
-You may assume that each input would have exactly one solution, and you may not use the same element twice.
+You may assume that each input would have exactly one solution, and you may not use the same element twice. */
 
 import java.util.*; 
 class Solution {
